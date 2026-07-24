2470c17cef14f1733744b0c14da32500a5193c91
thanisha3011 <thanisha3011@gmail.com>
2026-07-24 11:37:17 +0530

    Import models in app/models/__init__.py

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from .delivery_performance import DeliveryPerformance
+from .product_quality_evaluation import ProductQualityEvaluation
+from .communication_log import CommunicationLog
+from .service_rating import ServiceRating
+from .performance_history import PerformanceHistory
+from .vendor_ranking import VendorRanking
